1e1659bab0c23abc5040e255ea5d0388916f0e78
cmilne <cmilne@ideo.com>
2013-04-26 10:57:24 -0400

    Initial Commit

---
+++ imageCaptureExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  imageCaptureExample
+//
+//  Created by Chris Milne on 4/26/13.
+//  Copyright (c) 2013 Chris Milne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ imageCaptureExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  imageCaptureExample
+//
+//  Created by Chris Milne on 4/26/13.
+//  Copyright (c) 2013 Chris Milne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
